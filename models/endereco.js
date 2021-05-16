c5f01e8b06cf89939f442900e173668f4b3a42d7
Henrique <henriquedevb@gmail.com>
2021-05-16 12:11:29 -0300

    Att-domingo

---
+++ models/endereco.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+const Endereco = new mongoose.Schema({
+
+    cidade: {
+        type: String,
+        required: [true,
+            
+            "Necessario informar cidade do paciente."
+        ]
+    },
+    quadra: {
+        type: String,
+        required: [true, 
+            "Necessario informar endereço do paciente."
+        ]
+
+    },
+    rua: {
+        type: Number,
+        required: [true, 
+            "Necessario informar endereço do paciente."
+        ]
+    },
+    estado: {
+        type: String,
+        required: [true, 
+            "Necessario informar estado da residencia do paciente."
+        ]
+    },
+    cep: {
+        type: Number,
+        required: [true, 
+            "Necessario informar o CEP do paciente."]
+    },
+    complemento: {
+        type: Number,
+        required: false
+    }
+}, {
+    timestamps: true
+});
+
+export default mongoose.model('endereco', Endereco);
