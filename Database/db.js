a22e794244873b56c13dc3713c8fb06b152f4e3b
henriquedevb <henriquedevb@gmail.com>
2021-05-24 16:56:51 -0300

    teste

--- Database/db.js
+++ Database/db.js
@@ -1,22 +1,47 @@
 import mongoose from "mongoose";
 
-const uri = "mongodb+srv://root:user123@projeto-integrado.dkqn1.mongodb.net/pacientes?retryWrites=true&w=majority";
+const DB_PASS = process.env.DB_PASS;
+const DB_USER = process.env.DB_USER;
+const DB_NAME = process.env.DB_NAME;
+const DB_HOST = process.env.DB_HOST;
 
-  class Connection {
+// const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
+
+//   class Connection {
+//     constructor() {
+//         this.mongodb();
+//     }
+
+//     mongodb() {
+//         mongoose.connect(uri, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true
+//         }).then(() => {
+//             console.log("Conectado a base de dados com sucesso!");
+//         }).catch((exception) => {
+//             console.log("Erro ao conectar a base de dados." + exception);
+//         });
+//     }
+
+// }
+
+class Connection {
     constructor() {
-        this.mongodb();
+      this.mongodb();
     }
-
+    //uri: mongodb+srv://<user>:<password>@<host>/<database>
     mongodb() {
-        mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        }).then(() => {
-            console.log("Conectado a base de dados com sucesso!");
-        }).catch((exception) => {
-            console.log("Erro ao conectar a base de dados." + exception);
-        });
+      mongoose.connect(//(DB_HOST) ? remoto : local
+          (DB_HOST) ? `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority` 
+          : "mongodb://localhost/adsbackend", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }).then(() => {
+        console.log("Conexão com o MongoDb realizada com sucesso!");
+      }).catch((exception) => {
+        console.log("Erro: Conexão com MongoDB não foi realizada com sucesso" + exception);
+      });
     }
-
-}
+  }
+  
 export default new Connection();
